597a6a0f6c6c615ab91337e9d6b050ba933c097e
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-08-10 22:31:24 +0200

    Add codes index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,49 @@
 // Iteration 1: Names and Input
-//
+
+const hacker1 = "Fabio" ;
+console.log(`The driver's name is ${hacker1}`)
+
+const hacker2 = "JINO";
+console.log(`The navigator's name is ${hacker2}`)
+
+
 // Iteration 2: Conditionals
+
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.ou `)
+} else if 
+    (hacker1.length < hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker2.length} characters.ou `)
+} else {
+    (hacker1.length === hacker2.length)
+    console.log(`Wow, you both have equally long names, it has ${hacker1.length} characters.ou `)
+}
 
 
 // Iteration 3: Loops
+//3.3 Depending on the lexicographic order of the strings, print:
+//- The driver's name goes first.
+//- Yo, the navigator goes first definitely.
+//- What?! You both have the same name?
+
+
+const driveUpp = hacker1.toUpperCase()
+console.log(driveUpp.split('')) 
+const driveSplit = driveUpp.split('')
+console.log(driveSplit.join(' '))
+const naviSplit = hacker2.split('')
+const naviHack2 = naviSplit.reverse('')
+console.log(naviHack2.join(''))
+
+
+
+
+
+
+
+
+
+
+
+
+
